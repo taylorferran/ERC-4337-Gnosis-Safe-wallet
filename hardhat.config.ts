8003695868740eb67f4c52db729ece1f3a674a40
taylorferran <tailzpk@hotmail.co.uk>
2023-03-01 18:51:20 +0100

    Start unit testing

---
+++ hardhat.config.ts
@@ -0,0 +1,13 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      optimizer: { enabled: true, runs: 200 },
+    },
+  },
+};
+
+export default config;
